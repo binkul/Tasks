586d6f6daa6cef55dd339770eb835ac686eedb6e
binkul <jacekbinkul@gmail.com>
2020-02-12 13:13:25 +0100

    Rest API II theory

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,6 +12,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
 
     /**
      * can be @RequestMapping(method = RequestMethod.GET, value = "getTasks") or @GetMapping("getTasks")
@@ -18,7 +25,7 @@ public class TaskController {
      */
     @GetMapping("getTasks")
     List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskListDto(service.getAllTasks());
     }
 
     /**
