20b354e54d847a0905b4fc4ac8ce25d90a8c6e9d
binkul <jacekbinkul@gmail.com>
2020-02-27 15:11:16 +0100

    showtasks.bat modified

--- showtasks.bat
+++ showtasks.bat
@@ -5,11 +5,21 @@ echo Error with program compiling
 goto end
 
 :firefox
-start "crud" "%PROGRAMFILES%\Mozilla Firefox\firefox.exe" "http://localhost:8080/crud/v1/task/getTasks"
-if "%ERRORLEVEL%" == "0" goto finish
-echo.
-echo cannot run web browser
-goto end
+@rem Windows 10 or Windows 7
+if exist "%PROGRAMFILES%\Mozilla Firefox\firefox.exe" (
+    start "crud" "%PROGRAMFILES%\Mozilla Firefox\firefox.exe" "http://localhost:8080/crud/v1/task/getTasks"
+    if "%ERRORLEVEL%" == "0" goto finish
+    echo.
+    echo cannot run web browser
+    goto end
+) else (
+    start "crud" "%PROGRAMFILES% (x86)\Mozilla Firefox\firefox.exe" "http://localhost:8080/crud/v1/task/getTasks"
+    if "%ERRORLEVEL%" == "0" goto finish
+    echo.
+    echo cannot run web browser
+    goto end
+)
+
 
 :finish
 echo.
